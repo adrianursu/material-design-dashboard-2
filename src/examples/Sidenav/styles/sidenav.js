da68a413aeca3b4605f59b2b6a3d71fa0a2981f8
Ursu Adrian <uursu.adrian@gmail.com>
2021-11-19 12:41:24 +0200

    :recycle: Update(sidneav): Added dark background to default sidenav
    When window is resized, the dark background remain

--- src/examples/Sidenav/styles/sidenav.js
+++ src/examples/Sidenav/styles/sidenav.js
@@ -28,6 +28,7 @@ export default makeStyles(
       sidenav: {
         boxShadow: xxl,
         border: "none",
+        background: linearGradient(gradients.dark.main, gradients.dark.state),
         [breakpoints.up("xl")]: {
           background: ({ transparentSidenav, darkSidenav }) => {
             let finalValue;
