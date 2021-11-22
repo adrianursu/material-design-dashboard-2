7936011e2363dfaebb0fdb1be8f89b8c4bf47df5
Ursu Adrian <uursu.adrian@gmail.com>
2021-11-22 11:15:38 +0200

    :new: Added darkModeColors

---
+++ src/assets/theme/base/darkModeColors.js
@@ -0,0 +1,313 @@
+/**
+=========================================================
+* Material Design Dashboard 2 React 
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/Material-ui-dashboard-pro-material-ui
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Materialware.
+*/
+
+/**
+ * The base dark mode colors for the Material Design Dashboard 2 React.
+ * You can add new color using this file.
+ * You can customized the dark mode colors for the entire Material Design Dashboard 2 React using thie file.
+ */
+
+export default {
+  background: {
+    default: "#1a2035 !important",
+    state: "#202940",
+  },
+
+  text: {
+    main: "#ffffff",
+  },
+
+  transparent: {
+    main: "transparent",
+  },
+
+  white: {
+    main: "#ffffff",
+    focus: "#ffffff",
+  },
+
+  black: {
+    light: "#141414",
+    main: "#000000",
+    focus: "#000000",
+  },
+
+  primary: {
+    main: "#e91e63",
+    focus: "#ad0a87",
+  },
+
+  secondary: {
+    main: "#7b809a",
+    focus: "#96a2b8",
+  },
+
+  info: {
+    main: "#1A73E8",
+    focus: "#3acaeb",
+  },
+
+  success: {
+    main: "#4CAF50",
+    focus: "#95dc39",
+  },
+
+  warning: {
+    main: "#fb8c00",
+    focus: "#fcd652",
+  },
+
+  error: {
+    main: "#F44335",
+    focus: "#c70505",
+  },
+
+  light: {
+    main: "#e9ecef",
+    focus: "#e9ecef",
+  },
+
+  dark: {
+    main: "#344767",
+    focus: "#344767",
+  },
+
+  grey: {
+    100: "#f8f9fa",
+    200: "#e9ecef",
+    300: "#dee2e6",
+    400: "#ced4da",
+    500: "#adb5bd",
+    600: "#6c757d",
+    700: "#495057",
+    800: "#343a40",
+    900: "#212529",
+  },
+
+  gradients: {
+    primary: {
+      main: "#EC407A",
+      state: "#D81B60",
+    },
+
+    secondary: {
+      main: "#747b8a",
+      state: "#495361",
+    },
+
+    info: {
+      main: "#49a3f1",
+      state: "#1A73E8",
+    },
+
+    success: {
+      main: "#66BB6A",
+      state: "#43A047",
+    },
+
+    warning: {
+      main: "#FFA726",
+      state: "#FB8C00",
+    },
+
+    error: {
+      main: "#EF5350",
+      state: "#E53935",
+    },
+
+    light: {
+      main: "#EBEFF4",
+      state: "#CED4DA",
+    },
+
+    dark: {
+      main: "#42424a",
+      state: "#191919",
+    },
+  },
+
+  socialMediaColors: {
+    facebook: {
+      main: "#3b5998",
+      dark: "#344e86",
+    },
+
+    twitter: {
+      main: "#55acee",
+      dark: "#3ea1ec",
+    },
+
+    instagram: {
+      main: "#125688",
+      dark: "#0e456d",
+    },
+
+    linkedin: {
+      main: "#0077b5",
+      dark: "#00669c",
+    },
+
+    pinterest: {
+      main: "#cc2127",
+      dark: "#b21d22",
+    },
+
+    youtube: {
+      main: "#e52d27",
+      dark: "#d41f1a",
+    },
+
+    vimeo: {
+      main: "#1ab7ea",
+      dark: "#13a3d2",
+    },
+
+    slack: {
+      main: "#3aaf85",
+      dark: "#329874",
+    },
+
+    dribbble: {
+      main: "#ea4c89",
+      dark: "#e73177",
+    },
+
+    github: {
+      main: "#24292e",
+      dark: "#171a1d",
+    },
+
+    reddit: {
+      main: "#ff4500",
+      dark: "#e03d00",
+    },
+
+    tumblr: {
+      main: "#35465c",
+      dark: "#2a3749",
+    },
+  },
+
+  alertColors: {
+    primary: {
+      main: "#ec407a",
+      state: "#d81b60",
+      border: "#f8bcd0",
+    },
+
+    secondary: {
+      main: "#747b8a",
+      state: "#495361",
+      border: "#d7d9e1",
+    },
+
+    info: {
+      main: "#49a3f1",
+      state: "#1a73e8",
+      border: "#bad5f8",
+    },
+
+    success: {
+      main: "#66bb6a",
+      state: "#43A047",
+      border: "#c9e7cb",
+    },
+
+    warning: {
+      main: "#ffa726",
+      state: "#fb8c00",
+      border: "#feddb3",
+    },
+
+    error: {
+      main: "#ef5350",
+      state: "#e53935",
+      border: "#fcc7c2",
+    },
+
+    light: {
+      main: "#ebeff4",
+      state: "#ced4da",
+      border: "#fbfbfc",
+    },
+
+    dark: {
+      main: "#42424a",
+      state: "#191919",
+      border: "#c2c8d1",
+    },
+  },
+
+  badgeColors: {
+    primary: {
+      background: "#f883dd",
+      text: "#a3017e",
+    },
+
+    secondary: {
+      background: "#e4e8ed",
+      text: "#5974a2",
+    },
+
+    info: {
+      background: "#abe9f7",
+      text: "#08a1c4",
+    },
+
+    success: {
+      background: "#cdf59b",
+      text: "#67b108",
+    },
+
+    warning: {
+      background: "#fef5d3",
+      text: "#fbc400",
+    },
+
+    error: {
+      background: "#fc9797",
+      text: "#bd0000",
+    },
+
+    light: {
+      background: "#ffffff",
+      text: "#c7d3de",
+    },
+
+    dark: {
+      background: "#8097bf",
+      text: "#1e2e4a",
+    },
+  },
+
+  inputColors: {
+    borderColor: { main: "#d2d6da", focus: "#35d1f5" },
+    boxShadow: "#81e3f9",
+    error: "#fd5c70",
+    success: "#66d432",
+  },
+
+  sliderColors: {
+    thumb: { borderColor: "#d9d9d9" },
+  },
+
+  circleSliderColors: {
+    background: "#d3d3d3",
+  },
+
+  tabs: {
+    indicator: { boxShadow: "#ddd" },
+  },
+};
